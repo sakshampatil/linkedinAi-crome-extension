74af51dcefcc3ac30af30cb3d52316a4e2fb109a
Saksham Patil <sakshampatil815@gmail.com>
2024-07-04 05:54:52 +0530

    added ai icon

--- src/content.tsx
+++ src/content.tsx
@@ -1,12 +1,11 @@
+import { AiIcon } from "assets/icons"
 import cssText from "data-text:~style.css"
 import type { PlasmoCSConfig } from "plasmo"
-
-import { CountButton } from "~features/CountButton"
+import { useEffect, useState } from "react"
 
 export const config: PlasmoCSConfig = {
   matches: ["https://*.linkedin.com/*"]
 }
-
 export const getStyle = () => {
   const style = document.createElement("style")
   style.textContent = cssText
@@ -14,10 +13,46 @@ export const getStyle = () => {
 }
 
 const PlasmoOverlay = () => {
+  const [isFocused, setIsFocused] = useState<boolean>(false)
+  const [element, setElement] = useState<HTMLElement | null>(null)
+
+  useEffect(() => {
+    const handleFocusIn = (event: Event) => {
+      const target = event.target as HTMLElement
+      if (target && target.matches(".msg-form__contenteditable")) {
+        setIsFocused(true)
+        setElement(target)
+      }
+    }
+
+    const handleFocusOut = (event: Event) => {
+      const target = event.target as HTMLElement
+      if (target && target.matches(".msg-form__contenteditable")) {
+        setIsFocused(false)
+        setElement(null)
+      }
+    }
+
+    document.addEventListener("focusin", handleFocusIn)
+    document.addEventListener("focusout", handleFocusOut)
+    return () => {
+      document.removeEventListener("focusin", handleFocusIn)
+      document.removeEventListener("focusout", handleFocusOut)
+    }
+  }, [])
+
   return (
-    <div className="z-50 flex fixed top-32 right-8">
-      <CountButton />
-    </div>
+    isFocused &&
+    element !== null && (
+      <div
+        className={`cursor-pointer absolute `}
+        style={{
+          top: `${element.getBoundingClientRect().bottom - 40}px`,
+          left: `${element.getBoundingClientRect().right - 50}px`
+        }}>
+        <AiIcon className={""} />
+      </div>
+    )
   )
 }
 
